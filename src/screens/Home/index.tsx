e5799ba65b7e09b1a591c4178fc747aa3800fc83
Fernando Tiburcio <fernando.tiburcio@agriness.com>
2022-04-16 11:38:28 -0300

    refactor: finished home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import {
   Container,
-  Title
-} from './styles';
+  Header,
+  TotalCars,
+  HeaderContent,
+  CarList,
+} from "./styles";
 
-export function Home(){
+export function Home() {
+  const carData = 
+    {
+      brand: "porsche",
+      name: "Panamera",
+      rent: {
+        period: "Ao dia",
+        price: 340,
+      },
+      thumbnail:
+        "https://w7.pngwing.com/pngs/1014/228/png-transparent-2018-porsche-panamera-car-luxury-vehicle-porsche-911-porsche-compact-car-car-performance-car.png",
+    };
+
   return (
     <Container>
-      <Title>
-        HOME
-      </Title>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1,2,3,4,5,6,7,8,9]}
+        renderItem={({item}) => <Car data={carData} /> }
+        keyExtractor={item => String(item)}
+      />
     </Container>
   );
 }
