c7c0c836e4a255c3b98da5fd9751b0023d392a56
Fernando Tiburcio <fernando.tiburcio@agriness.com>
2022-04-20 22:00:26 -0300

    feat: add schedulingDetails screen

---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,128 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+import { Acessory } from "../../components/Acessory";
+import { Button } from "../../components/Button";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Acessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from "./styles";
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://w7.pngwing.com/pngs/1014/228/png-transparent-2018-porsche-panamera-car-luxury-vehicle-porsche-911-porsche-compact-car-car-performance-car.png",
+          ]}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Acessories>
+          <Acessory name="380km/h" icon={SpeedSvg} />
+          <Acessory name="3.2s" icon={AccelerationSvg} />
+          <Acessory name="800 HP" icon={ForceSvg} />
+          <Acessory name="Gasolina" icon={GasolineSvg} />
+          <Acessory name="Auto" icon={ExchangeSvg} />
+          <Acessory name="2 pessoas" icon={PeopleSvg} />
+        </Acessories>
+
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue>22/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>Total</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 1740</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title="Alugar Agora" color={theme.colors.success} />
+      </Footer>
+    </Container>
+  );
+}
